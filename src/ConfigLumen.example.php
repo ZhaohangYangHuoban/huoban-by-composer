9a9ef0438a3fcf35f472b518ed01f7fccef42bcd
ZhaohangYang <yangzhaohang@comsenz-service.com>
2021-09-07 14:00:06 +0800

    添加配置文件1

---
+++ src/ConfigLumen.example.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * @Author: SanQian
+ * @Date: 2021-09-07 13:57:53
+ * @LastEditTime: 2021-09-07 13:59:37
+ * @LastEditors: SanQian
+ * @Description:
+ * @FilePath: /huoban_tools_php/src/ConfigLumen.example.php
+ *
+ */
+
+// return [
+//     'huoban_pass' => [
+//         // 应用信息
+//         'application_id'     => env('HUOBAN_APP_ID'),
+//         'application_secret' => env('HUOBAN_APP_SECRET'),
+
+//         // 配置名称
+//         'name'               => 'huoban_pass',
+//         // 是否启用别名模式
+//         'alias_model'        => true,
+//         // 权限类别  enterprise/table
+//         'app_type'           => 'enterprise',
+//         // 工作区id
+//         'space_id'           => '4000000002101383',
+//         // api地址
+//         'api_url'            => 'https://api.huoban.com',
+//         // 上传文件地址
+//         'upload_url'         => 'https://upload.huoban.com',
+//         // 缓存文件存放地址
+//         'cache_path'         => storage_path() . '/huoban/',
+//     ],
+// ];
